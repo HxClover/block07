d34a2f959272a1e4a5df97391f292346ceee1fed
hxclover <rbecca.lam@gmail.com>
2023-10-30 16:38:14 -0700

    complete

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+
+const vaultMsg =
+	"You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Assign three variables.  Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/). Each must equal one of the three codes in the combination.
+
+let numOne = 2 * 5;
+let numTwo = 60 - 20;
+let numThree = 30 + 9;
+
+// Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+alert(vaultMsg + " " + numOne + " " + numTwo + " " + numThree);
